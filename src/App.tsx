187d4b34fb44b0a7863164312fff0edf3844ed1c
namyoonseo <ysnam0123@naver.com>
2025-04-29 17:17:37 +0900

    홈화면 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
+import Home from './pages/Home';
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl underline">App Component</h1>
+      <Home />
     </>
   );
 }
-
-// import { twMerge } from 'tailwind-merge';
-
-// export default function App() {
-//   const islogin = true;
-//   return (
-//     <>
-//       <h1
-//         className={twMerge('text-3xl', 'text-9xl', islogin && 'text-rose-500')}
-//       >
-//         Hello World!
-//       </h1>
-//     </>
-//   );
-// }
---
+++ src/hook/Homebackground.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import homeImage1 from '../assets/images/home1.png';
+import homeImage2 from '../assets/images/home2.png';
+
+const images = [homeImage1, homeImage2];
+
+export default function Homebackground() {
+  const [backgroundImage, setBackgroundImage] = useState(images[0]);
+
+  useEffect(() => {
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    setBackgroundImage(randomImage);
+  }, []);
+
+  return backgroundImage;
+}
