c2fdc9bd8092718d63da72a52c59279d7155ffd9
Ujin <tladbwls0430@gmail.com>
2025-04-30 15:04:55 +0900

    사이드바 동작 및 스타일 개선

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,28 @@
-import { twMerge } from 'tailwind-merge';
+import { twMerge } from "tailwind-merge";
 
-type OnlyLiterals<T> = T extends string ? (string extends T ? never : T) : never;
-type ReactInputType = OnlyLiterals<React.InputHTMLAttributes<HTMLInputElement>['type']>;
-type InputProps = Omit<React.ComponentPropsWithoutRef<'input'>, 'type'> & {
-    type?: Exclude<ReactInputType, 'radio' | 'checkbox' | 'range'>;
+type OnlyLiterals<T> = T extends string
+  ? string extends T
+    ? never
+    : T
+  : never;
+type ReactInputType = OnlyLiterals<
+  React.InputHTMLAttributes<HTMLInputElement>["type"]
+>;
+type InputProps = Omit<React.ComponentPropsWithoutRef<"input">, "type"> & {
+  type?: Exclude<ReactInputType, "radio" | "checkbox" | "range">;
 };
 
 export default function Input(props: InputProps) {
-    const { className, type = 'text', ...rest } = props;
+  let { className, type = "text", ...rest } = props;
 
-    return (
-        <input
-            type={type}
-            className={twMerge(
-                'w-full h-11 p-3 border-[1px] border-[var(--color-black)] rounded-[10px] mt-1 text-[var(--color-black)] text-base',
-                className
-            )}
-            {...rest}
-        />
-    );
+  return (
+    <input
+      type={type}
+      className={twMerge(
+        "w-full h-11 p-3 border-[1px] border-[var(--color-black)] rounded-[10px] mt-1 text-[var(--color-black)] text-base",
+        className
+      )}
+      {...rest}
+    />
+  );
 }
