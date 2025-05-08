1585006994e575c22ffafc0a3002be781fcf0c72
hansanga <cab08132@naver.com>
2025-05-08 15:28:53 +0900

    feat: - 정보수정페이지 퍼블리싱 수정 - 프로필 이미지 변경 기능 - 칭호 클릭 활성화

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -3,15 +3,18 @@ import { twMerge } from "tailwind-merge";
 type TagProps = {
   icon?: string;
   label: string;
-  className?: string;
+  selected?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
 };
 
-export default function Tag({ icon, label, className }: TagProps) {
+export default function Tag({ icon, label, selected, onClick }: TagProps) {
   return (
     <div
+      onClick={onClick}
       className={twMerge(
-        "w-42 h-8 border border-[#949494] rounded-[19px] text-[#949494] mx-0.5 my-0.5 flex items-center gap-1 px-5 cursor-pointer hover:text-[#60B5FF] hover:border-[#60B5FF] transition-all duration-200",
-        className
+        "w-42 h-8 border border-[#949494] rounded-[19px] text-[#949494] mx-0.5 my-0.5 flex items-center gap-1 px-5 cursor-pointer hover:text-[var(--color-main-skyBlue)] hover:border-[var(--color-main-skyBlue)] hover:border-2 transition-all duration-200",
+        selected &&
+          "border-[var(--color-main-skyBlue)] text-[var(--color-main-skyBlue)] border-2"
       )}
     >
       {icon && <img src={icon} className="w-5" alt="tag icon" />}
