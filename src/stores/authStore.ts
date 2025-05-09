4c82cf7ae2792663bcdf1277d95475550cfca214
TYss00 <rlaxodus203@naver.com>
2025-05-09 14:04:35 +0900

    feat: id값 추가, 로컬 스트리지에서 새션스토리지로 변경

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,23 +1,37 @@
 import { create } from 'zustand';
 
-const storedToken = localStorage.getItem('accessToken');
+// sessionStorage에 저장된 토큰값을 가져온다.
+const storedToken = sessionStorage.getItem('accessToken');
+const storedUserId = sessionStorage.getItem('userId');
 
 interface AuthStore {
     isLoggedIn: boolean;
     accessToken: string | null;
-    login: (accessToken: string) => void;
+    userId: string | null;
+    login: (accessToken: string, userId: string) => void;
     logout: () => void;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
+    // sessionStorage에 토큰이 있으면 true, 없으면 false
     isLoggedIn: !!storedToken,
+
+    // sessionStorage에서 토큰값 가져온다.
     accessToken: storedToken,
-    login: (accessToken: string) => {
-        localStorage.setItem('accessToken', accessToken);
-        set({ isLoggedIn: true, accessToken });
+
+    userId: storedUserId,
+
+    // 로그인할떄 토큰을받아서 sessionStorage에 저장해준다.
+    login: (accessToken: string, userId: string) => {
+        sessionStorage.setItem('accessToken', accessToken);
+        sessionStorage.setItem('userId', userId);
+        set({ isLoggedIn: true, accessToken, userId });
     },
+
+    // 로그아웃할떄 토큰을 sessionStorage에서 삭제한다.
     logout: () => {
-        localStorage.removeItem('accessToken');
-        set({ isLoggedIn: false, accessToken: null });
+        sessionStorage.removeItem('accessToken');
+        sessionStorage.removeItem('userId');
+        set({ isLoggedIn: false, accessToken: null, userId: null });
     },
 }));
