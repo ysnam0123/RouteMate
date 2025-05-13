7c67ef7beb57655b50a0cb59cd47b25321bddd0b
llyybbb <yb9766@naver.com>
2025-05-13 17:11:32 +0900

    first commit

---
+++ src/pages/Supervisor.tsx
@@ -0,0 +1,12 @@
+import Layout from '../layout/Layout'
+import Supervisor from './Supervisor'
+
+export default function Supervisor() {
+  return (
+    <>
+      <Layout>
+        <h1>Supervisor</h1>
+      </Layout>
+    </>
+  )
+}
