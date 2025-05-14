ae1770cf609d4e55582b3e283c6fe37d38bb7c72
namyoonseo <ysnam0123@naver.com>
2025-05-14 08:43:30 +0800

    basic 브랜치 기반으로 ysPractice 코드 적용

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,28 @@
+import Button from '../components/button';
+import NotFoundImg from '../assets/images/NotFound.svg';
+import { useNavigate } from 'react-router';
+export default function NotFound() {
+  const navigate = useNavigate();
+  const goToChannel = () => {
+    navigate('/Channel');
+  };
+  return (
+    <div className="flex flex-col justify-center items-center mt-[10%]">
+      <img
+        src={NotFoundImg}
+        alt="NotFoundImg"
+        className="w-[210px] h-[210px] mb-[70px]"
+      />
+      <h1 className="text-[54px] font-extrabold mb-[50px]">PAGE NOT FOUND</h1>
+      <p className="text-[var(--color-main-skyBlue)] text-[20px] font-bold mb-[15px]">
+        길을 잃으셨나요?
+      </p>
+      <Button
+        className="bg-[var(--color-main-navy)] w-[248px] h-[53px] text-[22px]"
+        onClick={goToChannel}
+      >
+        RouteMate와 길 찾기
+      </Button>
+    </div>
+  );
+}
