ecc1a564439e58a90fdb88900f136fff884cae24
llyybbb <yb9766@naver.com>
2025-05-14 14:18:34 +0900

    publish: 관리자 페이지 퍼블리싱 완료

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Header from './Header'
 import SidebarIsLogged from './SidebarIsLogged'
+import Header from './Header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <div className="flex flex-1 overflow-hidden">
         <SidebarIsLogged />
         <main className="flex-1 overflow-y-auto p-4">{children}</main>
