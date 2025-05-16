11029d6f847576920c8575e8baee9a9c6470f2e9
hansanga <cab08132@naver.com>
2025-05-16 10:21:10 +0900

    feat: 알림 토스트메세지로 변경, 로딩 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import Channel from './components/Channel'
-import { Routes, Route } from 'react-router-dom'
-import RootLayout from './layout/RootLayout'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import NotFound from './pages/NotFound'
-import ProfileEdit from './pages/ProfileEdit'
-import PublicRouter from './components/routers/PublicRouter'
-import ProtectedRouter from './components/routers/ProtectedRouter'
-import Home from './pages/Home'
-import UserProfile from './pages/UserProfile'
-import MyProfile from './pages/MyProfile'
-import Write from './pages/Write'
-import UserList from './pages/UserList'
-import SuperAdmin from './pages/SuperAdmin'
-import AdminRouter from './components/routers/AdminRouter'
+import Channel from './components/Channel';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
+import PublicRouter from './components/routers/PublicRouter';
+import ProtectedRouter from './components/routers/ProtectedRouter';
+import Home from './pages/Home';
+import UserProfile from './pages/UserProfile';
+import MyProfile from './pages/MyProfile';
+import Write from './pages/Write';
+import UserList from './pages/UserList';
+import SuperAdmin from './pages/SuperAdmin';
+import AdminRouter from './components/routers/AdminRouter';
+import Layout from './layout/Layout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function App() {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        closeOnClick
+        closeButton={false}
+      />
+
       <Routes>
         {/* 인증없이 사용가능한 라우터 */}
         <Route element={<PublicRouter />}>
@@ -25,32 +35,30 @@ export default function App() {
           <Route path="/login" element={<Login />} />
         </Route>
 
-        {/* 인증(로그인)이 있어야 사용가능한 라우터 */}
-        <Route element={<ProtectedRouter />}>
-          <Route element={<RootLayout />}>
+        <Route path="/layout" element={<Layout />} />
+
+        {/* 임시로 둔곳 */}
+        <Route element={<Layout />}>
+          <Route path="/channel" element={<Channel />} />
+          <Route path="/userprofile/:userId" element={<UserProfile />} />
+          <Route path="/userlist" element={<UserList />} />
+
+          {/* 인증(로그인)이 있어야 사용가능한 라우터 */}
+          <Route element={<ProtectedRouter />}>
             <Route path="/profileedit" element={<ProfileEdit />} />
             <Route path="/myprofile" element={<MyProfile />} />
             <Route path="/write" element={<Write />} />
           </Route>
-        </Route>
 
-        {/* SuperAdmin만 접근 가능한 관리자 페이지 */}
-        <Route path="/superadmin" element={<AdminRouter />}>
-          <Route element={<RootLayout />}>
+          {/* SuperAdmin만 접근 가능한 관리자 페이지 */}
+          <Route path="/superadmin" element={<AdminRouter />}>
             <Route index element={<SuperAdmin />} />
           </Route>
-        </Route>
-
-        {/* 임시로 둔곳 */}
-        <Route element={<RootLayout />}>
-          <Route path="/channel" element={<Channel />} />
-          <Route path="/userprofile/:userId" element={<UserProfile />} />
-          <Route path="/userlist" element={<UserList />} />
         </Route>
 
         {/* 없는 라우터면 NotFound */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
